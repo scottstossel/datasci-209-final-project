f95a2c154a3820d76e30d53005a316a6e0f4c742
Perry Gabriel <pgabriel@berkeley.edu>
2025-04-12 16:26:21 -0700

    add index and direct flights templates with enhanced layout and navigation

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['SERVER_NAME'] = 'localhost:5004'
 @app.route('/')
 def home():
-    return render_template('dashboard.html')  # template where Tableau is embedded
-
+    return render_template('index.html')  # template where Tableau is embedded
+@app.route('/direct-flights')
+def direct_flights():
+    return render_template('direct-flights.html')
+@app.route('/currency')
+def currency():
+    return render_template('currency.html')
 if __name__ == '__main__':
     app.run(debug=True)
