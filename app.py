bfff043175b7cfa1a96c7ceaf5386d9ad7e7d388
Perry Gabriel <pgabriel@berkeley.edu>
2025-04-12 16:05:02 -0700

    remove server name configuration from app settings

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SERVER_NAME'] = 'localhost:5001'
 @app.route('/')
 def home():
     return render_template('dashboard.html')  # template where Tableau is embedded
