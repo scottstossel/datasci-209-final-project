ae0366d92d73c40d9043e41a9e90198a466cd472
Perry Gabriel <pgabriel@berkeley.edu>
2025-04-12 16:01:27 -0700

    update server name in app configuration and enhance dashboard template with improved layout and styling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SERVER_NAME'] = 'localhost:5002'
+app.config['SERVER_NAME'] = 'localhost:5001'
 @app.route('/')
 def home():
     return render_template('dashboard.html')  # template where Tableau is embedded
