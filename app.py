7e117b9f82bc9d45cce9fbbb95c415a95f810cb0
Perry Gabriel <pgabriel@berkeley.edu>
2025-04-13 16:45:51 -0700

    Add About Us page and update navigation links

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ def tourism_overview():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-    
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(debug=True)
